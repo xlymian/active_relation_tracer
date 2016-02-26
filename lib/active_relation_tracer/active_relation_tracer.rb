502f9509995b8ca60f9f8afb927b0e33a40b9089
Paul Mylchreest <paul.mylchreest@mac.com>
2016-02-26 17:18:52 -0500

    Refactor with wrap_method module method

--- lib/active_relation_tracer/active_relation_tracer.rb
+++ lib/active_relation_tracer/active_relation_tracer.rb
@@ -4,36 +4,20 @@ module ActiveRelationTracer
   extend ActiveSupport::Concern
 
   included do
-    class_eval do
-      name = :where
-      original_method = instance_method(name)
+    [:where, :order, :limit].each do |method|
+      ActiveRelationTracer.wrap_method(self, method)
+    end
+  end
+
+  def self.wrap_method(klass, name)
+    klass.class_eval do
+      original_method = instance_method(name) # create unbound method with original
       define_method(name) do |*args, &block|
         trace = "Calling '#{name}' with '#{args.inspect}'"
         STDERR.puts trace
-        original_method.bind(self).call *args, &block
+        original_method.bind(self).call *args, &block # bind to self and call original
       end
     end
-
-    class_eval do
-      name = :order
-      original_method = instance_method(name)
-      define_method(name) do |*args, &block|
-        trace = "Calling '#{name}' with '#{args.inspect}'"
-        STDERR.puts trace
-        original_method.bind(self).call *args, &block
-      end
-    end
-
-    class_eval do
-      name = :limit
-      original_method = instance_method(name)
-      define_method(name) do |*args, &block|
-        trace = "Calling '#{name}' with '#{args.inspect}'"
-        STDERR.puts trace
-        original_method.bind(self).call *args, &block
-      end
-    end
-
   end
 
 end
