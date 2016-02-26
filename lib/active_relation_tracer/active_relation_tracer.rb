9273cf776b6286169e2a8f85dc8929edbaab5175
Paul Mylchreest <paul.mylchreest@mac.com>
2016-02-26 17:14:36 -0500

    Trace calls to :order and :limit

--- lib/active_relation_tracer/active_relation_tracer.rb
+++ lib/active_relation_tracer/active_relation_tracer.rb
@@ -13,6 +13,27 @@ module ActiveRelationTracer
         original_method.bind(self).call *args, &block
       end
     end
+
+    class_eval do
+      name = :order
+      original_method = instance_method(name)
+      define_method(name) do |*args, &block|
+        trace = "Calling '#{name}' with '#{args.inspect}'"
+        STDERR.puts trace
+        original_method.bind(self).call *args, &block
+      end
+    end
+
+    class_eval do
+      name = :limit
+      original_method = instance_method(name)
+      define_method(name) do |*args, &block|
+        trace = "Calling '#{name}' with '#{args.inspect}'"
+        STDERR.puts trace
+        original_method.bind(self).call *args, &block
+      end
+    end
+
   end
 
 end
--- spec/active_relation_tracer_spec.rb
+++ spec/active_relation_tracer_spec.rb
@@ -4,15 +4,24 @@ require_relative '../lib/active_relation_tracer/active_relation_tracer'
 class User < ActiveRecord::Base
 end
 
-describe ActiveRelationTracer do
-  before(:all) do
-    ActiveRecord::Relation.send(:include, ActiveRelationTracer)
-  end
+ActiveRecord::Relation.send(:include, ActiveRelationTracer)
 
+describe ActiveRelationTracer do
   it "traces calls to :where" do
     expected = %{Calling 'where' with '[{:first_name=>"John"}]'}
     expect(STDERR).to receive(:puts).once.with(expected)
-    User.where(first_name: "John").first
-    expect(true).to be_truthy
+    User.where(first_name: "John")
+  end
+
+  it "traces calls to :order" do
+    expected = %{Calling 'order' with '[{:first_name=>:desc}]'}
+    expect(STDERR).to receive(:puts).once.with(expected)
+    User.order(first_name: :desc)
+  end
+
+  it "traces calls to :limit" do
+    expected = %{Calling 'limit' with '[1]'}
+    expect(STDERR).to receive(:puts).once.with(expected)
+    User.limit(1)
   end
 end
